bd0f59fe76010c34c36d4e3135a1935f2a551bde
MAYANK DIXIT <44857089+mayankdixit029@users.noreply.github.com>
2025-10-03 22:47:18 +0530

    seo google tags canges

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,7 @@ export const metadata = {
     title: "Currency Strength Meter | Real-Time Forex Strength Analysis",
     description:
       "Track live forex currency strength with our free Currency Strength Meter. Instantly compare the strongest and weakest currencies in real-time to boost your forex trading strategies and improve market analysis.",
-    images: ["https://www.currencystrengthsmeters.com//og-image.png"],
+    images: ["https://www.currencystrengthsmeters.com/og-image.png"],
   },
   icons: {
     icon: "/favicon.ico",
@@ -53,8 +53,18 @@ export default function RootLayout({ children }) {
       {/* ✅ Next.js injects metadata here automatically */}
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
-
-        {/* Google AdSense script should be placed here */}
+        {/* Google Tag Manager */}
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M3ZWWFT8');</script>
+       {/* End Google Tag Manager  */}
+       {/* Google Tag Manager (noscript) */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3ZWWFT8"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      {/* End Google Tag Manager (noscript) */}
+      {/* Google AdSense script should be placed here */}
         <Script
           id="adsense-script"
           async
