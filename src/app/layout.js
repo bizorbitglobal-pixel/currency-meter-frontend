b5f63751f6bcef329114517b018f8fc84ec44e6b
MAYANK DIXIT <44857089+mayankdixit029@users.noreply.github.com>
2025-10-05 13:53:54 +0530

    google tag manager script

--- src/app/layout.js
+++ src/app/layout.js
@@ -299,6 +299,24 @@ export default function RootLayout({ children }) {
             },
           })}
         </Script>
+        {/* ✅ Google Tag Manager (Script) */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M3ZWWFT8');
+            `,
+          }}
+        />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
@@ -339,6 +357,18 @@ export default function RootLayout({ children }) {
         <Script id="adsense-init" strategy="afterInteractive">
           {`(adsbygoogle = window.adsbygoogle || []).push({});`}
         </Script>
+         {/* ✅ Google Tag Manager (noscript fallback) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M3ZWWFT8"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
       </body>
     </html>
   );
