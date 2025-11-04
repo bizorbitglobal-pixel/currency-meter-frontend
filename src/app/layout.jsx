0c09787fb4c523a6f4888f40f586d005287f13d0
MAYANK DIXIT <44857089+mayankdixit029@users.noreply.github.com>
2025-11-04 22:12:56 +0530

    update description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,12 +12,12 @@ const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"]
 
 export const metadata = {
   title:
-    "Currency Strength Meter - Live Forex Strength Indicator (2025)",
+    "Currency Strength Meter - Live New Forex Strength Indicator (2025)",
   description:
     "Currency Strength Meter is a live forex strength indicator that helps traders to identify the strongest and weakest currencies in real time.It exists for MT4, MT5, TradingView, and as a web-based tool. We can help you improve your forex trading strategy by providing accurate currency strength analysis.",
   keywords: keywords,
   openGraph: {
-    title: "Currency Strength Meter - Live Forex Strength Indicator (2025)",
+    title: "Currency Strength Meter - Live New Forex Strength Indicator (2025)",
     description:
       "Currency Strength Meter is a live forex strength indicator that helps traders to identify the strongest and weakest currencies in real time.It exists for MT4, MT5, TradingView, and as a web-based tool. We can help you improve your forex trading strategy by providing accurate currency strength analysis.",
     url: "https://www.currencystrengthsmeters.com/",
@@ -32,7 +32,7 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Currency Strength Meter - Live Forex Strength Indicator (2025)",
+    title: "Currency Strength Meter - Live New Forex Strength Indicator (2025)",
     description:
       "Currency Strength Meter is a live forex strength indicator that helps traders to identify the strongest and weakest currencies in real time.It exists for MT4, MT5, TradingView, and as a web-based tool. We can help you improve your forex trading strategy by providing accurate currency strength analysis.",
     images: ["https://www.currencystrengthsmeters.com/og-image.png"],
